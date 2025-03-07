b3e42593b88ca28871f73bfcf925cf1ee7c5001d
Marlowe Oliver <marloweoliver@protonmail.com>
2025-03-07 13:00:04 -1000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="flex flex-row min-h-screen justify-center items-center">
+			<h1>Hello World!</h1>
+		</main>
+	);
+}
